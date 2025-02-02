b660f5ff061aeda415b184a07b2d8d7f508b5ed7
VitorHugo05 <vitorhugo.pozzi@gmail.com>
2025-02-02 10:24:02 -0300

    fix: fixing incompatible version of spring boot

--- hr-worker/src/main/java/com/vitordev/hrworker/repositories/WorkerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.vitordev.hrworker.repositories;
-
-import com.vitordev.hrworker.entities.Worker;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WorkerRepository extends JpaRepository<Worker, Long> {
-}
