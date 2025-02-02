6e5fbed64ae0f728d7afab9290cc48f25710cd2f
VitorHugo05 <vitorhugo.pozzi@gmail.com>
2025-02-02 13:35:11 -0300

    Project hr-server-config created

---
+++ hr-server-config/src/main/java/com/vitordev/hrserverconfig/HrServerConfigApplication.java
@@ -0,0 +1,13 @@
+package com.vitordev.hrserverconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrServerConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrServerConfigApplication.class, args);
+	}
+
+}
