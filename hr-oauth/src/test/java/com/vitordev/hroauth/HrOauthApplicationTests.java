9954f6e40057556564f040834f3af667cfaa7d3f
VitorHugo05 <vitorhugo.pozzi@gmail.com>
2025-02-05 20:56:33 -0300

    Project hr-oauth created

---
+++ hr-oauth/src/test/java/com/vitordev/hroauth/HrOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitordev.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
